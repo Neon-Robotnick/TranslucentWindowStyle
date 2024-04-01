256acfe2b126f3068f2af9a7202690b5cb6ad89a
Neon-Robotnick <144548090+Neon-Robotnick@users.noreply.github.com>
2024-04-01 23:35:20 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["TranslucentWindowStyle"]),
     ],
     dependencies: [
-    .package(url: "https://github.com/DominatorVbN/TranslucentWindowBackgroundStyle", .upToNextMinor(from: "1.0.0"))
+    .package(url: https://github.com/Neon-Robotnick/TranslucentWindowStyle", .upToNextMinor(from: "1.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
